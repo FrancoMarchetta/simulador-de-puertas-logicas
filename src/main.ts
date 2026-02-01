025a1edfd59dbc1fb9a8d74124810080de6c0489
FrancoMarchetta <frncmarchetta@gmail.com>
2026-02-01 16:13:17 -0300

    Tables

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,13 @@ let $gateSelect = document.getElementById("gate-select") as HTMLInputElement;
 let $output = document.getElementById("output") as HTMLElement ;
 let $input1 = document.getElementById("input1") as HTMLInputElement ;
 let $input2 = document.getElementById("input2") as HTMLInputElement ;
-let Table = document.getElementById("Table") as HTMLImageElement;
+let $input2Label = document.getElementById("input2-label") as HTMLElement;
+let $truthTable = document.getElementById("truth-table") as HTMLElement;
 
 let gate:string = $gateSelect.value;
 
 $gateSelect.addEventListener("change", () => {
-  gate = $gateSelect.value;
-
-    
+  gate = $gateSelect.value;    
   console.log(gate);
   CheckGateState()
 });
@@ -31,10 +30,45 @@ $output.addEventListener("change", ()=>{
 })
 
 function CheckGateState(){
+    
+    // Show/hide second input based on gate type
+    if (gate === "NOT") {
+        $input2Label.classList.add("hidden");
+    } else {
+        $input2Label.classList.remove("hidden");
+    }
 
 switch(gate){
     case "AND" :
-    Table.src = "/public/Images/ANDT.png";
+        $truthTable.innerHTML = `
+            <table>
+                <tr>
+                    <th>A</th>
+                    <th>B</th>
+                    <th>A ∧ B</th>
+                </tr>
+                <tr>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td>0</td>
+                    <td>1</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>0</td>
+                    <td>0</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                </tr>
+            </table>
+        `;
         if($input1.checked && $input2.checked){
             $output.innerHTML = "1";
             $output.classList.remove("bulb-off");
@@ -50,7 +84,143 @@ switch(gate){
 
         }
         break;
+        case "NOT" :
+            $truthTable.innerHTML = `
+                <table>
+                    <tr>
+                        <th>A</th>
+                        <th>¬A</th>
+                    </tr>
+                    <tr>
+                        <td>0</td>
+                        <td>1</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>0</td>
+                    </tr>
+                </table>
+            `;
+            if(!$input1.checked){
+                $output.innerHTML = "1";
+                $output.classList.remove("bulb-off");
+                $output.classList.add("bulb-on");
+            }else{
+                $output.innerHTML = "0";
+                $output.classList.remove("bulb-on");
+                $output.classList.add("bulb-off");
+            }
+            break;
+        case "NAND" :
+            $truthTable.innerHTML = `
+                <table>
+                    <tr>
+                        <th>A</th>
+                        <th>B</th>
+                        <th>¬(A ∧ B)</th>
+                    </tr>
+                    <tr>
+                        <td>0</td>
+                        <td>0</td>
+                        <td>1</td>
+                    </tr>
+                    <tr>
+                        <td>0</td>
+                        <td>1</td>
+                        <td>1</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>0</td>
+                        <td>1</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>0</td>
+                    </tr>
+                </table>
+            `;
+            if(!($input1.checked && $input2.checked)){
+                $output.innerHTML = "1";
+                $output.classList.remove("bulb-off");
+                $output.classList.add("bulb-on");
+            }else{
+                $output.innerHTML = "0";
+                $output.classList.remove("bulb-on");
+                $output.classList.add("bulb-off");
+            }
+            break;
+        case "NOR" :
+            $truthTable.innerHTML = `
+                <table>
+                    <tr>
+                        <th>A</th>
+                        <th>B</th>
+                        <th>¬(A ∨ B)</th>
+                    </tr>
+                    <tr>
+                        <td>0</td>
+                        <td>0</td>
+                        <td>1</td>
+                    </tr>
+                    <tr>
+                        <td>0</td>
+                        <td>1</td>
+                        <td>0</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>0</td>
+                        <td>0</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>0</td>
+                    </tr>
+                </table>
+            `;
+            if(!($input1.checked || $input2.checked)){
+                $output.innerHTML = "1";
+                $output.classList.remove("bulb-off");
+                $output.classList.add("bulb-on");
+            }else{
+                $output.innerHTML = "0";
+                $output.classList.remove("bulb-on");
+                $output.classList.add("bulb-off");
+            }
+            break;
         case "OR" :
+            $truthTable.innerHTML = `
+                <table>
+                    <tr>
+                        <th>A</th>
+                        <th>B</th>
+                        <th>A ∨ B</th>
+                    </tr>
+                    <tr>
+                        <td>0</td>
+                        <td>0</td>
+                        <td>0</td>
+                    </tr>
+                    <tr>
+                        <td>0</td>
+                        <td>1</td>
+                        <td>1</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>0</td>
+                        <td>1</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                    </tr>
+                </table>
+            `;
             if($input1.checked || $input2.checked){
                 $output.innerHTML = "1";
                 $output.classList.remove("bulb-off");
@@ -66,7 +236,77 @@ switch(gate){
             break;
 
             case "XOR" :
+                $truthTable.innerHTML = `
+                    <table>
+                        <tr>
+                            <th>A</th>
+                            <th>B</th>
+                            <th>A ⊕ B</th>
+                        </tr>
+                        <tr>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>0</td>
+                        </tr>
+                        <tr>
+                            <td>0</td>
+                            <td>1</td>
+                            <td>1</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>0</td>
+                            <td>1</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>1</td>
+                            <td>0</td>
+                        </tr>
+                    </table>
+                `;
                 if($input1.checked != $input2.checked){
+                    $output.innerHTML = "1";
+                    $output.classList.remove("bulb-off");
+                    $output.classList.add("bulb-on");
+                }
+                else{
+                    $output.innerHTML = "0";
+                    $output.classList.remove("bulb-on");
+                    $output.classList.add("bulb-off");
+                }
+            break;
+            case "XNOR" :
+                $truthTable.innerHTML = `
+                    <table>
+                        <tr>
+                            <th>A</th>
+                            <th>B</th>
+                            <th>¬(A ⊕ B)</th>
+                        </tr>
+                        <tr>
+                            <td>0</td>
+                            <td>0</td>
+                            <td>1</td>
+                        </tr>
+                        <tr>
+                            <td>0</td>
+                            <td>1</td>
+                            <td>0</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>0</td>
+                            <td>0</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>1</td>
+                            <td>1</td>
+                        </tr>
+                    </table>
+                `;
+                if($input1.checked == $input2.checked){
                     $output.innerHTML = "1";
                     $output.classList.remove("bulb-off");
                     $output.classList.add("bulb-on");
